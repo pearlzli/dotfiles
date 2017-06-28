eae90098714de1cd1b83a5c142309d46d3707402
Pearl Li <pearlzli16@gmail.com>
2017-06-28 15:05:33 -0400

    First pass at initialization script

---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### 1. Link dotfiles
+
+cd ~
+for file in ".bashrc" ".tmux.conf" ".emacs" ".emacs-modes.el" ".gitconfig"; do
+    ln -s "dotfiles/$file" "$file"
+    echo "Linked $file"
+done
+
+
+### 2. Clone necessary packages into .emacs.d
+
+if [ ! -d "~/.emacs.d" ]; then
+    mkdir "~/.emacs.d"
+    echo "Created .emacs.d"
+fi
+
+cd "~/.emacs.d"
+
+# cl-lib
+wget "https://elpa.gnu.org/packages/cl-lib-0.5.el"
+
+# Git
+git clone "https://github.com/magit/git-modes.git"
+
+# Julia
+git clone "https://github.com/JuliaEditorSupport/julia-emacs.git"
+
+# Markdown
+git clone "https://github.com/defunkt/markdown-mode.git"
+
+# MATLAB
+wget "matlab-emacs.cvs.sourceforge.net/viewvc/matlab-emacs/matlab-emacs/?view=tar"
+tar -zxvf "matlab-emacs?view\=tar"
+rm "matlab-emacs?view=tar"
