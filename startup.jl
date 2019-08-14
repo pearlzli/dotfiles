204f093fca54155a13f5fd93db943a8e20d12bc6
Pearl Li <pearlzli16@gmail.com>
2019-08-14 01:29:46 -0700

    Fix loading Revise.jl on startup

--- startup.jl
+++ startup.jl
@@ -1,5 +1,14 @@
 # Load Revise.jl
-using Revise
+# See https://timholy.github.io/Revise.jl/stable/config/#Using-Revise-by-default-1
+atreplinit() do repl
+    try
+        @eval using Revise
+        @async Revise.wait_steal_repl_backend()
+        @info "Loaded Revise"
+    catch ex
+        @warn ex
+        @warn "Revise not loaded"
+    end
 
 # Load project-specific packages
 if isfile("Project.toml")
