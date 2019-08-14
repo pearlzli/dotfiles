973749a277eb1c07464737dd05f2cd397097b6f8
Pearl Li <pearlzli16@gmail.com>
2019-08-14 01:31:04 -0700

    Stop calling activate/instantiate on Julia startup

--- startup.jl
+++ startup.jl
@@ -9,11 +9,4 @@ atreplinit() do repl
         @warn ex
         @warn "Revise not loaded"
     end
-
-# Load project-specific packages
-if isfile("Project.toml")
-    using Pkg
-    Pkg.activate(".")
-    Pkg.instantiate()
-    println("  Activated and instantiated current project directory: $(basename(pwd()))")
 end
