7495f09842e7bec49cf18dc32167075112e759af
Pearl Li <pearlzli16@gmail.com>
2020-05-01 15:45:54 -0700

    Remove require, use auto-mode-alist and with-eval-after-load

--- .emacs-modes.el
+++ .emacs-modes.el
@@ -15,19 +15,21 @@
   (set-face-foreground 'font-latex-italic-face "brightred")
   (set-face-foreground 'font-latex-math-face "brightyellow"))
 
-(setq TeX-parse-self t) ; make AUCTeX run bibtex
-(setq TeX-auto-save t)  ; https://emacs.stackexchange.com/a/13870/14500
-(defun my-TeX-auto-generate ()
-  "Auto-generate and reapply style hooks"
-  (TeX-auto-generate "~/dotfiles/tex/latex" "~/dotfiles/tex/latex/auto") ; https://tex.stackexchange.com/a/410552/116532
-  'TeX-normal-mode) ; https://www.gnu.org/software/auctex/manual/auctex/Parsing-Files.html
-(setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer (https://emacs.stackexchange.com/a/7573/14500)
+(with-eval-after-load 'TeX-latex-mode
+  (setq TeX-parse-self t) ; make AUCTeX run bibtex
+  (setq TeX-auto-save t)  ; https://emacs.stackexchange.com/a/13870/14500
+  (setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer (https://emacs.stackexchange.com/a/7573/14500)
+  (defun my-TeX-auto-generate ()
+    "Auto-generate and reapply style hooks"
+    (TeX-auto-generate "~/dotfiles/tex/latex" "~/dotfiles/tex/latex/auto") ; https://tex.stackexchange.com/a/410552/116532
+    'TeX-normal-mode)) ; https://www.gnu.org/software/auctex/manual/auctex/Parsing-Files.html
 
 (setq reftex-extra-bindings t) ; use more intuitive key bindings (must be defined before loading RefTeX)
 (require 'reftex)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
-(setq reftex-default-bibliography '("~/Drive/research/papers/library.bib")) ; set default bib file (https://tex.stackexchange.com/a/54825/116532)
+(with-eval-after-load 'reftex-mode
+  (setq reftex-plug-into-AUCTeX t)
+  (setq reftex-default-bibliography '("~/Drive/research/papers/library.bib"))) ; set default bib file (https://tex.stackexchange.com/a/54825/116532)
 
 ;; Git
 (add-to-list 'load-path "~/.emacs.d/git-modes")
@@ -42,32 +44,29 @@
 (autoload 'gfm-mode "gfm-mode" "Major mode for editing GitHub Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-(setq markdown-asymmetric-header t)
-(setq markdown-enable-math t)
-(defun my-markdown-mode-hook ()
-  "Custom Markdown hook"
+(with-eval-after-load 'gfm-mode
+  (setq markdown-asymmetric-header t)
+  (setq markdown-enable-math t)
   (local-unset-key (kbd "M-n"))  ; I redefined M-n and M-p in .emacs
   (local-unset-key (kbd "M-p"))) ; https://stackoverflow.com/a/19324726/2756250
-(add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
 
 ;; Julia
 (add-to-list 'load-path "~/.emacs.d/julia-emacs")
-(require 'julia-mode)
+(add-to-list 'auto-mode-alist '("\\.jl\\'" . julia-mode))
+(autoload 'julia-mode "julia-mode" "Julia mode" t)
 
 ;; MATLAB
 (add-to-list 'load-path "~/.emacs.d/matlab-emacs")
 (autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
 (autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
 (setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
-(setq matlab-indent-function t)
-(setq matlab-comment-region-s "% ")
-(defun my-matlab-mode-hook ()
-  "Custom MATLAB hook"
-  (turn-on-auto-fill)
+(with-eval-after-load 'matlab-mode
   (setq fill-column 80)
+  (setq matlab-indent-function t)
+  (setq matlab-comment-region-s "% ")
+  (turn-on-auto-fill)
   (local-set-key "\M-;" nil)
   (local-set-key "\M-q" nil))
-(add-hook 'matlab-mode-hook 'my-matlab-mode-hook)
 
 ;; Stata
 (add-to-list 'load-path "~/.emacs.d/ado-mode/lisp")
@@ -82,4 +81,5 @@
 
 ;; ESS (R only)
 (add-to-list 'load-path "~/.emacs.d/ESS/lisp")
-(require 'ess-r-mode)
+(autoload 'ess-r-mode "ess-r-mode" "R mode (ESS)" t)
+(setq auto-mode-alist (cons '("\\.r$" . ess-r-mode) auto-mode-alist))
