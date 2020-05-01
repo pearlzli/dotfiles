6b421fe910e5156fc3432ba9ab4c28a268857682
Pearl Li <pearlzli16@gmail.com>
2020-05-01 13:42:35 -0700

    Clean up .emacs-modes.el

--- .emacs-modes.el
+++ .emacs-modes.el
@@ -2,11 +2,10 @@
 (setq TeX-parse-self t) ; make AUCTeX run bibtex
 (setq TeX-auto-save t)  ; https://emacs.stackexchange.com/a/13870/14500
 (defun my-TeX-auto-generate ()
-  "Custom LaTeX hook"
+  "Auto-generate and reapply style hooks"
   (TeX-auto-generate "~/dotfiles/tex/latex" "~/dotfiles/tex/latex/auto") ; https://tex.stackexchange.com/a/410552/116532
   'TeX-normal-mode) ; https://www.gnu.org/software/auctex/manual/auctex/Parsing-Files.html
-(setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer
-                                          ; https://emacs.stackexchange.com/a/7573/14500
+(setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer (https://emacs.stackexchange.com/a/7573/14500)
 
 (setq reftex-extra-bindings t) ; use more intuitive key bindings (must be defined before loading RefTeX)
 (require 'reftex)
@@ -16,24 +15,22 @@
 
 ;; Git
 (add-to-list 'load-path "~/.emacs.d/git-modes")
-(autoload 'gitconfig-mode "gitconfig-mode"
-  "Major mode for editing .gitconfig files" t)
 (add-to-list 'auto-mode-alist '("\\.gitconfig\\'" . gitconfig-mode))
-(autoload 'gitignore-mode "gitignore-mode"
-  "Major mode for editing .gitignore files" t)
 (add-to-list 'auto-mode-alist '("\\.gitignore\\'" . gitignore-mode))
+(autoload 'gitconfig-mode "gitconfig-mode" "Major mode for editing .gitconfig files" t)
+(autoload 'gitignore-mode "gitignore-mode" "Major mode for editing .gitignore files" t)
 
 ;; Markdown
 (add-to-list 'load-path "~/.emacs.d/markdown-mode")
 (load "markdown-mode.el")
-(autoload 'gfm-mode "gfm-mode"
-   "Major mode for editing GitHub Flavored Markdown files" t)
+(autoload 'gfm-mode "gfm-mode" "Major mode for editing GitHub Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-(add-hook 'markdown-mode-hook
-          (lambda()
-            (local-unset-key (kbd "M-n"))   ; M-n and M-p redefined in .emacs
-            (local-unset-key (kbd "M-p")))) ; https://stackoverflow.com/a/19324726/2756250
+(defun my-markdown-mode-hook ()
+  "Custom Markdown hook"
+  (local-unset-key (kbd "M-n"))  ; I redefined M-n and M-p in .emacs
+  (local-unset-key (kbd "M-p"))) ; https://stackoverflow.com/a/19324726/2756250
+(add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
 
 ;; Julia
 (add-to-list 'load-path "~/.emacs.d/julia-emacs")
@@ -42,19 +39,17 @@
 ;; MATLAB
 (add-to-list 'load-path "~/.emacs.d/matlab-emacs")
 (autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
+(autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
 (setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
+(setq matlab-indent-function t)
+(setq matlab-comment-region-s "% ")
 (defun my-matlab-mode-hook ()
   "Custom MATLAB hook"
-  (setq matlab-indent-function t) ; function bodies indented
-  (setq fill-column 80) ; where auto-fill should wrap
   (turn-on-auto-fill)
-  (setq matlab-comment-region-s "% "))
-(autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
-(defun my-matlab-shell-mode-hook ()
-  '())
+  (setq fill-column 80)
+  (local-set-key "\M-;" nil)
+  (local-set-key "\M-q" nil))
 (add-hook 'matlab-mode-hook 'my-matlab-mode-hook)
-(add-hook 'matlab-mode-hook (lambda () (local-set-key "\M-;" nil)))
-(add-hook 'matlab-mode-hook (lambda () (local-set-key "\M-q" nil)))
 
 ;; Stata
 (add-to-list 'load-path "~/.emacs.d/ado-mode/lisp")
