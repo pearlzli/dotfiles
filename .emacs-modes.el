449390b52d28ac73c4c8cefa8e37e377e7c88aa4
Pearl Li <pearlzli16@gmail.com>
2020-05-01 16:40:28 -0700

    Don't go overboard using with-eval-after-load

--- .emacs-modes.el
+++ .emacs-modes.el
@@ -15,21 +15,20 @@
   (set-face-foreground 'font-latex-italic-face "brightred")
   (set-face-foreground 'font-latex-math-face "brightyellow"))
 
-(with-eval-after-load 'TeX-latex-mode
-  (setq TeX-parse-self t) ; make AUCTeX run bibtex
-  (setq TeX-auto-save t)  ; https://emacs.stackexchange.com/a/13870/14500
-  (setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer (https://emacs.stackexchange.com/a/7573/14500)
-  (defun my-TeX-auto-generate ()
-    "Auto-generate and reapply style hooks"
-    (TeX-auto-generate "~/dotfiles/tex/latex" "~/dotfiles/tex/latex/auto") ; https://tex.stackexchange.com/a/410552/116532
-    'TeX-normal-mode)) ; https://www.gnu.org/software/auctex/manual/auctex/Parsing-Files.html
+(setq TeX-parse-self t) ; make AUCTeX run bibtex
+(setq TeX-auto-save t)  ; https://emacs.stackexchange.com/a/13870/14500
+(setq LaTeX-beamer-item-overlay-flag nil) ; don't ask for itemize overlay in Beamer (https://emacs.stackexchange.com/a/7573/14500)
+(setq bibtex-align-at-equal-sign t)
+(defun my-TeX-auto-generate ()
+  "Auto-generate and reapply style hooks"
+  (TeX-auto-generate "~/dotfiles/tex/latex" "~/dotfiles/tex/latex/auto") ; https://tex.stackexchange.com/a/410552/116532
+  'TeX-normal-mode) ; https://www.gnu.org/software/auctex/manual/auctex/Parsing-Files.html
 
 (setq reftex-extra-bindings t) ; use more intuitive key bindings (must be defined before loading RefTeX)
 (require 'reftex)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(with-eval-after-load 'reftex-mode
-  (setq reftex-plug-into-AUCTeX t)
-  (setq reftex-default-bibliography '("~/Drive/research/papers/library.bib"))) ; set default bib file (https://tex.stackexchange.com/a/54825/116532)
+(setq reftex-default-bibliography '("~/Drive/research/papers/library.bib")) ; set default bib file (https://tex.stackexchange.com/a/54825/116532)
+(setq reftex-plug-into-AUCTeX t)
 
 ;; Git
 (add-to-list 'load-path "~/.emacs.d/git-modes")
@@ -44,9 +43,9 @@
 (autoload 'gfm-mode "gfm-mode" "Major mode for editing GitHub Flavored Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+(setq markdown-asymmetric-header t)
+(setq markdown-enable-math t)
 (with-eval-after-load 'gfm-mode
-  (setq markdown-asymmetric-header t)
-  (setq markdown-enable-math t)
   (local-unset-key (kbd "M-n"))  ; I redefined M-n and M-p in .emacs
   (local-unset-key (kbd "M-p"))) ; https://stackoverflow.com/a/19324726/2756250
 
@@ -60,10 +59,10 @@
 (autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
 (autoload 'matlab-shell "matlab" "Interactive Matlab mode." t)
 (setq auto-mode-alist (cons '("\\.m$" . matlab-mode) auto-mode-alist))
+(setq matlab-indent-function t)
+(setq matlab-comment-region-s "% ")
 (with-eval-after-load 'matlab-mode
   (setq fill-column 80)
-  (setq matlab-indent-function t)
-  (setq matlab-comment-region-s "% ")
   (local-set-key "\M-;" nil)
   (local-set-key "\M-q" nil))
 
@@ -71,9 +70,9 @@
 (add-to-list 'load-path "~/.emacs.d/ado-mode/lisp")
 (autoload 'ado-mode "ado-mode" "Stata mode (ado-mode)" t)
 (setq auto-mode-alist (cons '("\\.do$" . ado-mode) auto-mode-alist))
+(setq ado-close-under-line-flag nil) ; indent close brace at same indent level as open brace
+(setq ado-tab-width 4)
 (with-eval-after-load 'ado-mode
-  (setq ado-close-under-line-flag nil) ; indent close brace at same indent level as open brace
-  (setq ado-tab-width 4)
   (define-key ado-mode-map (kbd "C-c C-c") 'ado-send-command-to-stata)
   (define-key ado-mode-map (kbd "C-c C-a") 'ado-send-buffer-to-stata)
   (define-key ado-mode-map (kbd "TAB") 'ado-indent-region)) ; default is ado-indent-line
