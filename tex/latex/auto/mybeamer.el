701d7f917d4ad86655f65ab023b12c2a3d41c044
Pearl Li <pearlzli16@gmail.com>
2021-02-06 14:37:50 -0800

    Add href to LaTeX-verbatim-macros-with-braces-local in mybeamer.el

--- tex/latex/auto/mybeamer.el
+++ tex/latex/auto/mybeamer.el
@@ -14,6 +14,7 @@
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
