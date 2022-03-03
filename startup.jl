81da42f34418c80dc03124492983b82ce2bcd015
Pearl Li <pearlzli16@gmail.com>
2022-03-02 18:00:55 -0800

    Update Revise loading

--- startup.jl
+++ startup.jl
@@ -1,12 +1,7 @@
 # Load Revise.jl
 # See https://timholy.github.io/Revise.jl/stable/config/#Using-Revise-by-default-1
-atreplinit() do repl
-    try
-        @eval using Revise
-        @async Revise.wait_steal_repl_backend()
-        @info "Loaded Revise"
-    catch ex
-        @warn ex
-        @warn "Revise not loaded"
-    end
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception=(e, catch_backtrace())
 end
