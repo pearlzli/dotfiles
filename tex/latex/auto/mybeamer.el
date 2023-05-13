40975860581d977a004fb1cc75fde1936ccd19fd
Pearl Li <pearlzli16@gmail.com>
2023-05-13 15:32:47 -0700

    Automatic update to mybeamer.el

--- tex/latex/auto/mybeamer.el
+++ tex/latex/auto/mybeamer.el
@@ -2,34 +2,49 @@
  "mybeamer"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("beamer" "aspectratio=169" "notheorems")))
+                     '(("beamer" "aspectratio=169" "notheorems" "10pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("lato" "default")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
     "beamer10"
     "lato"
+    "natbib"
     "appendixnumberbeamer"
     "mystyle")
    (TeX-add-symbols
+    '("citet" ["argument"] 1)
+    '("citep" ["argument"] 1)
+    '("cite" ["argument"] 1)
+    '("slideselection" 1)
+    '("blmb" 2)
+    '("blg" 2)
+    '("bl" 2)
     '("theoremcite" 1)
-    "myvskip")
+    '("letbeamertemplate" 2)
+    "miniframeson"
+    "miniframesoff"
+    "disablecolorlinks"
+    "oldcite"
+    "oldcitep"
+    "oldcitet")
    (LaTeX-add-environments
-    '("wideitemize" LaTeX-env-item)
-    '("wideenumerate" LaTeX-env-item))
+    '("handoutframeselect" LaTeX-env-args ["argument"] 0)
+    "wideitemize"
+    "wideenumerate")
    (LaTeX-add-amsthm-newtheorems
     "assump"
     "claim"
@@ -44,6 +59,12 @@
    (LaTeX-add-xcolor-definecolors
     "indigo"
     "blue"
-    "pink"))
+    "pink"
+    "lightgray"
+    "replightgrey"
+    "repmedgrey"
+    "reporange"
+    "repdarkpurple"
+    "themepurple"))
  :latex)
 
