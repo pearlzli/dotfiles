534022be9f995f5b25cd15b28acfeb5eb3caff99
Pearl Li <pearlzli16@gmail.com>
2025-11-13 15:36:01 -0500

    Fix and document elpa 'failed to download archive' bugs

--- elpa-install.el
+++ elpa-install.el
@@ -1,8 +1,20 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (when (version< emacs-version "26.3")
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+  ; Fix "Failed to download 'melpa' archive" error
+  ; https://melpa.org/#/getting-started (under "Known Issues")
+  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+  ; Fix "Failed to download 'gnu' archive" error
+  ; https://stackoverflow.com/a/58236306
+  ; https://elpa.gnu.org/packages/gnu-elpa-keyring-update.html
+  (unless (package-installed-p 'gnu-elpa-keyring-update)
+    (setq package-check-signature nil)
+    (package-install 'gnu-elpa-keyring-update)
+    (setq package-check-signature t)))
+
 (package-refresh-contents)
 
 (mapcar (lambda (package)
